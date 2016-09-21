4bfb9f530c33e4e0e761d6fb428d615540eb420c
rochebox <rochebox@gmail.com>
2016-09-21 01:05:51 +0000

    go to here to go back to user authentication

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,12 +1,15 @@
 class NotificationMailer < ActionMailer::Base
   default from: "support@villamadeleine.com"
 
-  def comment_added
-  	mail(to: "rochebox@gmail.com",
-  		subject: "A comment has been added to your place")
+ # *** use rochebox+EmailTest111@gmail.com
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place.user
+  	mail(to: @place_owner.email,
+  		subject: "A comment has been added to #{@place.name}")
   end
 
-  def sign_up_confirmation
+  def sign_up_confirmation(user_email)
   	mail(to: "rochebox@gmail.com",
   		subject: "Please confirm you sign up at Villa Madeleine Homeowners Association")
   end
