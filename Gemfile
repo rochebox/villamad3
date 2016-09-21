4a93c2bd51dd24182264405f6db450b7cc1129ee
rochebox <rochebox@gmail.com>
2016-09-21 00:01:04 +0000

    Added Figaro from Lesson 29 Nomster and then lesson 41

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bootstrap-sass', '~> 3.3.5.1'
 gem 'sprockets', '2.11.0'
 gem 'sass-rails'
 gem 'devise', '>= 3.2.4'
+gem "figaro", ">= 1.0.0"
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,12 +2,12 @@ class NotificationMailer < ActionMailer::Base
   default from: "support@villamadeleine.com"
 
   def comment_added
-  	mail(to: "christopherroche@stmarksschool.org",
+  	mail(to: "rochebox@gmail.com",
   		subject: "A comment has been added to your place")
   end
 
   def sign_up_confirmation
-  	mail(to: "christopherroche@stmarksschool.org",
+  	mail(to: "rochebox@gmail.com",
   		subject: "Please confirm you sign up at Villa Madeleine Homeowners Association")
   end
 end
