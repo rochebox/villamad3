00d530d319aee5315086df72f5f4e8097158a11b
rochebox <rochebox@gmail.com>
2016-09-28 00:54:13 +0000

    make restaurant table like lesson 5 nomster

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160922041122) do
+ActiveRecord::Schema.define(version: 20160928005007) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "address"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
