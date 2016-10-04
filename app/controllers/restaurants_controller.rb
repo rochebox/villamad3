4721bc0c30005a2e750ca32b80c757fe06827a2a
rochebox <rochebox@gmail.com>
2016-10-04 23:21:21 +0000

    Adding restaurant Index Nomster Lesson 6

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,18 @@
 class RestaurantsController < ApplicationController
 
 	def index
+		@restaurants = Restaurant.all
 	end
+
+	def new
+		@restaurant = Restaurant.new
+	end
+
+	def create
+		
+		restaurant = Restaurant.create(name: params[:name] )
+		redirect_to restaurants_path
+	end
+
+
 end
