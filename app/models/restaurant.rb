b71c71226af235eb5ef0b586a412f23054212026
rochebox <rochebox@gmail.com>
2016-10-11 21:00:48 +0000

    Adding Restaurant Comments Nomster Lesson 31

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ActiveRecord::Base
 	belongs_to :user
+	has_many :comments
+	
 	validates :name, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :restaurants
+  has_many :comments
 end
